d51bb3e4307eeb1d257fcc32edb850971c3f3bb5
AJ Ibraheem <aibrahee@mathworks.com>
2022-10-18 17:26:15 +0100

    Points preprocessing

--- +traced_model2/+ops/pyAtenNorm.m
+++ +traced_model2/+ops/pyAtenNorm.m
@@ -4,7 +4,7 @@ function varargout = pyAtenNorm(varargin)
 % Other parameters in `varargin{2:end}` are the parameters of the pytorch
 % call the equivalent `torch.norm(descriptor, p=2, dim=1)`
 inputTensor = varargin{1}; 
-descriptor = extractdata(inputTensor.value); % [1×256×60×80 dlarray] -> BxCxSxS
+descriptor = inputTensor.value; %extractdata(inputTensor.value); % [1×256×60×80 dlarray] -> BxCxSxS
 
 % Reimplement norm
 sumSqDesc = sum(descriptor .^ 2, 2); % sum along `C`
@@ -12,6 +12,6 @@ channelNorm = sqrt(sumSqDesc); % sqrt of a 1x1x60x80 matrix
 normalisedMatrix = descriptor ./ channelNorm;
 
 % Assign the norm into varargout
-result = struct('value', dlarray(normalisedMatrix), 'rank', inputTensor.rank);
+result = struct('value', normalisedMatrix, 'rank', inputTensor.rank);
 varargout{1} = result;
 end
