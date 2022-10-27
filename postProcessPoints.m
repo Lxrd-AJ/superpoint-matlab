4aa6abbe02366a75fddda21589252c273949d619
AJ Ibraheem <aibrahee@mathworks.com>
2022-10-27 09:54:17 +0100

    Updated post processing step for interest points

--- postProcessPoints.m
+++ postProcessPoints.m
@@ -1,10 +1,14 @@
-function [points, heatmap] = postProcessPoints(pts, opts)
+function [points, heatmaps] = postProcessPoints(pts, opts)
     % postProcessPoints 
+    % Returns 
+    %   `points` :  A 3xN matrix of points with the format [x_i, y_i, confidence]^T
+    %   `heatmap`:  A HxW matrix in the range [0,1] of interest point confidence across entire image
     arguments
         %   pts: A dlarray with format SSCB
         %        For a given input image of size 480x640, `pts` should be `60(S)x80(S)x65(C)x1(B)`
         pts
         opts.DownsamplingFactor = 8
+        opts.ConfidenceThreshold = 0.015
     end
     
     points = softmax(pts);
@@ -13,6 +17,7 @@ function [points, heatmap] = postProcessPoints(pts, opts)
     % plus an extra "no interest point" dustbin
     points = points(:,:,1:end-1,:); % This extra dustbin (last channel) is removed
 
+    % --- HEATMAP ---
     % The network predicts interest points for every pixel in the image
     % This can be visualised as a heatmap
     % The heatmap can also be thresholded to derive useable interest points
@@ -38,9 +43,34 @@ function [points, heatmap] = postProcessPoints(pts, opts)
     % Resize into original image size i.e H(S)xW(S)x1(C)xN(B)
     H = sz(1) * patchSize;
     W = sz(2) * patchSize;
-    heatmap = reshape(heatmap, H, W, 1, []);
+    heatmaps = reshape(heatmap, H, W, 1, []);
 
+    % -- POINTS --
     % Filtering
-    % Remove points with low confidence scores
-    
+    filteredHeatmaps = heatmaps;
+    filteredHeatmaps(filteredHeatmaps <= opts.ConfidenceThreshold) = 0;
+    if ndims(filteredHeatmaps) == 4
+        numInBatch = size(filteredHeatmaps, 4);
+        % Extract keypoints for each batch
+        points = cell(1,numInBatch);
+        for k=1:numInBatch
+            currentHeatmap = filteredHeatmaps(:,:,:,k);
+            [yCoords, xCoords, ptConfidence] = find(extractdata(currentHeatmap));
+            points{k} = horzcat(yCoords, xCoords, ptConfidence);
+        end
+    else
+        [yCoords, xCoords, ptConfidence] = find(extractdata(filteredHeatmaps));
+        points = horzcat(yCoords, xCoords, ptConfidence);
+    end
+end
+
+function [suppressedPoints, pointIndices] = iNonMaximumSuppression(points, imageSize, opts)
+    % iNonMaximumSuppression
+    % Filter out interest points that are close to each based on the threshold  specified in `opts.nmsDistance`
+    arguments
+        points
+        imageSize
+        opts.nmsDistance = 4
+    end
+    % TODO: Continue
 end
